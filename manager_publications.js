b20ece6fcbf87168f45abb71aa4f3d963f610a82
Glenda Hannibal <glendahannibal@gmail.com>
2025-08-24 14:22:02 +0200

    publication page update

--- manager_publications.js
+++ manager_publications.js
@@ -1,61 +1,126 @@
-// Robust BibTeX parser
+// --- Simple BibTeX parser ---
 function parseBibTeX(text) {
     const entries = [];
-    const entryRegex = /@(\w+)\s*{\s*([^,]+),/g;
-    let match, lastIndex = 0;
+    const entryRegex = /@(\w+)\s*{\s*([^,]+),([\s\S]*?)\n}/g;
+    let match;
 
     while ((match = entryRegex.exec(text)) !== null) {
-        const entryType = match[1];
+        const entryType = match[1].toLowerCase();
         const citationKey = match[2];
-        const start = entryRegex.lastIndex;
-        let braceCount = 0;
-        let end = start;
+        const fieldsText = match[3];
 
-        // Find matching closing brace for this entry
-        for (let i = start; i < text.length; i++) {
-            if (text[i] === '{') braceCount++;
-            if (text[i] === '}') {
-                if (braceCount === 0) {
-                    end = i;
-                    break;
-                } else {
-                    braceCount--;
-                }
-            }
-        }
-
-        const fieldsText = text.substring(start, end);
         const fields = {};
-        // Regex to capture field = {value} or field = "value"
-        const fieldRegex = /(\w+)\s*=\s*(\{([^{}]*(?:\{[^}]*\}[^}]*)*)\}|"([^"]*)")/g;
+        const fieldRegex = /(\w+)\s*=\s*(?:"([^"]*)"|\{([^{}]*)\})/g;
         let fieldMatch;
+
         while ((fieldMatch = fieldRegex.exec(fieldsText)) !== null) {
-            fields[fieldMatch[1].toLowerCase()] = fieldMatch[3] || fieldMatch[4];
+            fields[fieldMatch[1].toLowerCase()] = fieldMatch[2] || fieldMatch[3];
         }
 
         entries.push({ entryType, citationKey, fields });
-        lastIndex = end;
     }
-
     return entries;
 }
 
-// Render references in HTML
-function renderReferences(entries) {
-    const list = document.getElementById('publications');
-    entries.forEach(entry => {
-        const f = entry.fields;
-        const li = document.createElement('li');
-        li.textContent = `${f.author || ""}. ${f.title || ""}. ${f.journal || f.publisher || ""}, ${f.year || ""}.`;
-        list.appendChild(li);
-    });
+// --- Format entry as HTML ---
+function formatEntry(entry) {
+    const f = entry.fields;
+    switch (entry.entryType) {
+        case "article":
+            return `${f.author || ""}. <em>${f.title || ""}</em>. ${f.journal || ""}, ${f.year || ""}.`;
+        case "book":
+            return `${f.author || ""}. <em>${f.title || ""}</em>. ${f.publisher || ""}, ${f.year || ""}.`;
+        case "inproceedings":
+            return `${f.author || ""}. <em>${f.title || ""}</em>. In ${f.booktitle || ""}, ${f.year || ""}.`;
+        default:
+            return `${f.author || ""}. <em>${f.title || ""}</em>. ${f.year || ""}.`;
+    }
 }
 
-// Fetch and parse .bib file
-fetch('publications.bib')
+// --- Render publications grouped by type ---
+function renderPublications(entries) {
+    const container = document.getElementById("publications");
+
+    // Group by type
+    const groups = {
+        article: [],
+        inproceedings: [],
+        book: [],
+        misc: []
+    };
+
+    entries.forEach(e => {
+        if (groups[e.entryType]) {
+            groups[e.entryType].push(e);
+        } else {
+            groups.misc.push(e);
+        }
+    });
+
+    // Sort each group by year (desc)
+    for (const type in groups) {
+        groups[type].sort((a, b) => (b.fields.year || 0) - (a.fields.year || 0));
+    }
+
+    // Render groups
+    if (groups.article.length) {
+        const sec = document.createElement("div");
+        sec.innerHTML = "<h2>Journal Articles</h2>";
+        const ul = document.createElement("ul");
+        groups.article.forEach(e => {
+            const li = document.createElement("li");
+            li.innerHTML = formatEntry(e);
+            ul.appendChild(li);
+        });
+        sec.appendChild(ul);
+        container.appendChild(sec);
+    }
+
+    if (groups.inproceedings.length) {
+        const sec = document.createElement("div");
+        sec.innerHTML = "<h2>Conference Papers</h2>";
+        const ul = document.createElement("ul");
+        groups.inproceedings.forEach(e => {
+            const li = document.createElement("li");
+            li.innerHTML = formatEntry(e);
+            ul.appendChild(li);
+        });
+        sec.appendChild(ul);
+        container.appendChild(sec);
+    }
+
+    if (groups.book.length) {
+        const sec = document.createElement("div");
+        sec.innerHTML = "<h2>Books</h2>";
+        const ul = document.createElement("ul");
+        groups.book.forEach(e => {
+            const li = document.createElement("li");
+            li.innerHTML = formatEntry(e);
+            ul.appendChild(li);
+        });
+        sec.appendChild(ul);
+        container.appendChild(sec);
+    }
+
+    if (groups.misc.length) {
+        const sec = document.createElement("div");
+        sec.innerHTML = "<h2>Other</h2>";
+        const ul = document.createElement("ul");
+        groups.misc.forEach(e => {
+            const li = document.createElement("li");
+            li.innerHTML = formatEntry(e);
+            ul.appendChild(li);
+        });
+        sec.appendChild(ul);
+        container.appendChild(sec);
+    }
+}
+
+// --- Load publication.bib ---
+fetch("publications.bib")
     .then(res => res.text())
     .then(text => {
         const entries = parseBibTeX(text);
-        renderReferences(entries);
+        renderPublications(entries);
     })
     .catch(err => console.error("Error loading BibTeX file:", err));
