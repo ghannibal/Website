31b2a854eaf373e49d31fe239d3f16dd9d17cee7
Glenda Hannibal <glendahannibal@gmail.com>
2026-03-20 07:02:33 +0100

    Update manager_fixed_headerfooter.js

--- manager_fixed_headerfooter.js
+++ manager_fixed_headerfooter.js
@@ -10,7 +10,7 @@ class FixedHeader extends HTMLElement {
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-            <link rel="icon" type="image/png" href="favicon.png">
+            <link rel="shortcut icon" href="http://glendahannibal/favicon.ico" />
             <title>Dr. Glenda Hannibal</title>
             <style>
                 body {
