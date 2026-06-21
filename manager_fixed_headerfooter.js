b92c4a41c2e76509ac25f2db6de112d63bc0bba9
Glenda Hannibal <glendahannibal@gmail.com>
2026-06-21 19:46:29 +0200

    Update manager_fixed_headerfooter.js

--- manager_fixed_headerfooter.js
+++ manager_fixed_headerfooter.js
@@ -7,7 +7,6 @@ class FixedHeader extends HTMLElement {
             <link rel="stylesheet" href="Pico_css/pico.min.css">
             <link rel="stylesheet" href="Pico_css/pico.colors.min.css">
             <link rel="stylesheet" href="custom_style.css">
-            <head>
             <link rel="icon" type="image/x-icon" href="favicon.ico">
             <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
             <link rel="apple-touch-icon" sizes="180x180" href="favicon-180x180.png">     
